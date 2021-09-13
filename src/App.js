89dc4fcad7cd874d429fc3be44406f8eba9d6748
Камила Потеева <ares909@yandex.ru>
2021-09-13 14:11:26 +0300

    linters ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import React from "react";
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, it's me</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello, it&apos;s me</h1>
+        </div>
+    );
 }
 
 export default App;
